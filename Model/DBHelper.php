b01ea1433a184d9c8a683361cac9b692bf4e0289
Haich <Haich@Haich-Hub96.mshome.net>
2018-06-19 13:33:16 +0200

    Implemented MVC Framework

---
+++ Model/DBHelper.php
@@ -0,0 +1,16 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of DBHelper
+ *
+ * @author Haich
+ */
+class DBHelper {
+    //put your code here
+}
