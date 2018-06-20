59b5a13ae1109bae7c2bb08dfa5573547909677d
Haich <Haich@Haich-Hub96.mshome.net>
2018-06-20 16:37:20 +0200

    End of day commit -  2018/06/20

--- Model/admin_model.php
+++ Model/admin_model.php
@@ -11,9 +11,107 @@
  *
  * @author Haich
  */
+include'../Model/DBHelper.php';
 class admin_model {
-    //put your code here
-    
-    
  
+    
+  /*Region sponsors*/  
+    public function get_sponsors() {
+        $stored_procedure ="CALL usp";
+        return DBHelper::GetAll($stored_procedure);  
+    }
+    
+    public function add_sponsor($spoName, $spoWebsite, $spoPicture) {
+        $stored_procedure ="CALL usp";
+        $params = array(
+         "" => $spoName,
+        ""=> $spoWebsite,
+        "" => $spoPicture
+        );
+        return DBHelper::Execute($stored_procedure, $params);
+    }
+     public function edit_sponsor($spoID,$spoName, $spoWebsite, $spoPicture) {
+         $stored_procedure ="CALL usp";
+         $params = array(
+        "" => $spoID,
+        "" => $spoName,
+        ""=> $spoWebsite,
+        "" => $spoPicture
+        );
+         return DBHelper::Execute($stored_procedure, $params);
+    }
+    
+    public function get_sponsor($spoID) {
+        $stored_procedure ="CALL usp";
+        $params = array(
+         "" => $spoID   
+        );
+        return DBHelper::GetRow($stored_procedure, $params);
+    }
+    /*End sponsors*/
+    
+   
+    /*Region: Events */
+    public function add_event($eveName, $eveStartDate, $eveAddress, $eveDescription, $eveEndDate) {
+        $stored_procedure ="CALL usp";
+        $params = array(
+        "" => $eveName,
+        "" => $eveStartDate,
+        "" => $eveAddress,
+        "" => $eveDescription,
+        "" => $eveEndDate
+        );
+        return DBHelper::Execute($stored_procedure, $params);
+    }
+
+    public function edit_event($eveID,$eveName, $eveStartDate, $eveAddress, $eveDescription, $eveEndDate) {
+        $stored_procedure ="CALL usp";
+        $params = array(
+        ""=> $eveID,
+        "" => $eveName,
+        "" => $eveStartDate,
+        "" => $eveAddress,
+        "" => $eveDescription,
+        "" => $eveEndDate
+        );
+        return DBHelper::Execute($stored_procedure, $params);
+    }
+    public function get_events() {
+        $stored_procedure ="CALL usp";
+        return DBHelper::GetAll($stored_procedure);
+    }
+    
+    public function get_event($eveID) {
+        $stored_procedure ="CALL usp";
+        $params = array(
+         "" => $eveID
+        );
+
+        return DBHelper::GetRow($stored_procedure, $params); 
+    }   
+    /*End Events Region*/
+ 
+    
+    /*Region: */
+    
+    /* End news coverage */
+    
+    
+    /*Region: Gallery */
+    public function add_picture($param) {
+        $stored_procedure="CALL usp";
+        $params = array(
+            
+        );
+        return DBHelper::Execute($stored_procedure, $params);
+    }
+    
+    public function remove_picture($param) {
+       $stored_procedure = "CALL usp";
+       $params = array(
+           
+       );
+       return DBHelper::Execute($sql, $params);
+    }
+    /*End Gallery*/
 }
