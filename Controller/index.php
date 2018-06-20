5855efeb316abe6b548ea0714984eef6503b459c
Haich <Haich@Haich-Hub96.mshome.net>
2018-06-20 12:22:44 +0200

    Added controller files

--- Controller/index.php
+++ Controller/index.php
@@ -11,6 +11,32 @@
  *
  * @author Haich
  */
-class index {
-    //put your code here
+require_once('../model/database_handler.php');
+require_once('../model/music_model.php');
+
+
+$action = filter_input(INPUT_POST, 'action');
+if ($action == NULL) {
+    $action = filter_input(INPUT_GET, 'action');
+    if ($action == NULL) {
+        $action = '';
+    }
 }
+
+switch($action) {
+	case 'list_products':
+		$category_ID = filter_input(INPUT_GET, 'category_id', 
+            FILTER_SANITIZE_STRING);
+		if ($category_ID == NULL || $category_ID == FALSE) {
+			$category_ID = "1";
+		}
+		$categories = get_categories();
+		$products = get_products_by_category($category_ID);
+		$category_name = get_category_name($category_ID);
+		include('../view/product_list.php');
+		break;
+	
+	case 'show_add_form':
+		include ('../view/office_add.php');
+		break;
+}//End switch 
