ad808348870936fa5cd7f2f4be02f70ade6511de
Shervin <shervindouwdies@gmail.com>
2018-06-20 14:37:03 +0200

    Changed Files To php and made a few changes

--- Controller/index.php
+++ Controller/index.php
@@ -11,32 +11,37 @@
  *
  * @author Haich
  */
-require_once('../model/database_handler.php');
-require_once('../model/music_model.php');
+require_once('../model/DBHelper.php');
+require_once('../model/admin_model.php');
 
 
 $action = filter_input(INPUT_POST, 'action');
 if ($action == NULL) {
     $action = filter_input(INPUT_GET, 'action');
     if ($action == NULL) {
-        $action = '';
+        $action = 'Vendor_details';
     }
 }
 
 switch($action) {
-	case 'list_products':
-		$category_ID = filter_input(INPUT_GET, 'category_id', 
+	case 'Vendor_details':
+		/*$category_ID = filter_input(INPUT_GET, 'category_id', 
             FILTER_SANITIZE_STRING);
 		if ($category_ID == NULL || $category_ID == FALSE) {
 			$category_ID = "1";
 		}
 		$categories = get_categories();
 		$products = get_products_by_category($category_ID);
-		$category_name = get_category_name($category_ID);
-		include('../view/product_list.php');
+		$category_name = get_category_name($category_ID);*/
+		include('../View/Vendor_details.php');
 		break;
 	
-	case 'show_add_form':
-		include ('../view/office_add.php');
+	case 'Item Details':
+		include ('../View/Item Details.php');
 		break;
+
+		case 'EditVendor':
+		include '../View/Edit_Vendor.php';
+
+			break;
 }//End switch 
